b1a26fa91b854dadf3dc623a7d5812476deee00b
yared <yared.amercha@gmail.com>
2022-02-03 00:48:27 -0500

    evrey thing changed

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-const a = document.getElementById('')
+const images = [
+    "./images/img-1.jpg",
+    "./images/img-2.jpg",
+    "./images/img-3.jpg",
+    "./images/img-4.jpg",
+    "./images/img-5.jpg"
+];
+
+
+
+const coverImage = document.getElementById('profile-picture')
+const modalPage = document.getElementById('modal')
+const firstPicture = document.getElementById('profile-picture')
+const nextPicture = document.getElementById('next-button')
+const backPicture = document.getElementById('back-button')
+
+
+let imageIndex =1
+function imageChanging(){
+    if(imageIndex < images.length){
+        coverImage.src = images[imageIndex]
+        imageIndex++;
+
+
+    }
+    else{
+        imageIndex = 0;
+        coverImage.src = images[imageIndex]
+    }
+}
+setInterval(imageChanging, 3000);
